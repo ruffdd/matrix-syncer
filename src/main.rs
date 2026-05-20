a592bc8200a76f71cc71076461aca6db679b4de4
David <develop@ruffdd.de>
2026-05-20 22:59:02 +0200

    improve licence notices

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-/* Copyright (C) 2025-2026  David Ruff develop@ruffdd.de
+/* Matrix user Syncer
+ * Copyright (C) 2025-2026  David Ruff develop@ruffdd.de
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -10,8 +11,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 use anyhow::{Error, Ok};
